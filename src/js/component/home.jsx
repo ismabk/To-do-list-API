752da0d40a64b3f6b6afb4b0543bbb821b145aa5
ismabk <ismabk94@gmail.com>
2021-12-02 05:43:01 +0100

    structure of the proyect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Home = () => {
-	//hooks
-	const [color, setColor] = useState("");
-
-	const changeColor = colors => {
-		setColor(colors);
-	};
-
-	return (
-		<React.Fragment>
-			<div className="box" style={{ background: color }}></div>
-			<button
-				type="button"
-				className="btn btn-success"
-				onClick={() => changeColor("green")}>
-				Verde
-			</button>
-			<button
-				type="button"
-				className="btn btn-warning"
-				onClick={() => changeColor("yellow")}>
-				Amarillo
-			</button>
-			<button
-				type="button"
-				className="btn btn-danger"
-				onClick={() => changeColor("red")}>
-				Rojo
-			</button>
-		</React.Fragment>
-	);
+	return <></>;
 };
 
 export default Home;
