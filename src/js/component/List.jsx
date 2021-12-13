a2f7040875212f504d8e4b7660dcd5ae96d620fa
ismabk <ismabk94@gmail.com>
2021-12-14 00:52:45 +0100

    maked todo with API

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 
 import "../../styles/List.scss";
 import ListItem from "./ListItem.jsx";
 
-const List = props => {
+const List = () => {
 	const [data, setData] = useState("");
 	const [list, setList] = useState([]);
-
+	const [placeh, setPlaceh] = useState("To do...");
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/Buga", {
 			method: "GET",
@@ -16,24 +15,20 @@ const List = props => {
 			}
 		})
 			.then(resp => {
-				console.log(resp.ok);
-				console.log(resp.status);
 				return resp.json();
 			})
 			.then(data => {
-				setList(data);
-				return data;
+				let dataFalse = data.filter(task => !task.done);
+				setList(dataFalse);
+				return dataFalse;
 			})
-			.catch(error => {
-				console.log(error);
-			});
-	}, []);
-
-	const optimizedFetch = (fetchMethod, fetchBody) => {
-		const url = "https://assets.breatheco.de/apis/fake/todos/user/Emanuel";
+			.catch(error => console.error(error));
+	}, [list]);
+	const putPostFetch = (fMethod, fBody) => {
+		const url = "https://assets.breatheco.de/apis/fake/todos/user/Buga";
 		const header = {
-			method: fetchMethod,
-			body: fetchBody,
+			method: fMethod,
+			body: fBody,
 			headers: {
 				"Content-Type": "application/json"
 			}
@@ -44,32 +39,49 @@ const List = props => {
 			.then(data => console.log(data))
 			.catch(error => console.error(error));
 	};
-
+	// function to remove tasks from the list
+	const handleClik = i => {
+		list[i].done = true;
+		putPostFetch("PUT", JSON.stringify(list));
+	};
 	// function to update the tasks in the list
 	const handleSubmit = ev => {
 		ev.preventDefault();
-		console.log(ev.value);
-		setList([...list, data.label]);
+		let myList = list;
+		myList.push({ label: data, done: false });
+		setList(myList);
+		putPostFetch("PUT", JSON.stringify(list));
 	};
-	// function to remove tasks from the list
-	const handleClik = i => {
-		setList(list.filter((val, position) => position !== i));
+	const deleteAll = () => {
+		for (let val in list) {
+			list[val].done = true;
+		}
+		setList(list);
+		putPostFetch("PUT", JSON.stringify(list));
 	};
-
 	return (
 		<>
 			<div id="myDIV" className="header ">
 				<h2>My To Do List</h2>
-				<form onSubmit={ev => handleSubmit(ev)}>
+				<form
+					onSubmit={ev => {
+						handleSubmit(ev);
+						ev.target.reset();
+						setPlaceh("another to do...");
+					}}>
 					<div className="d-flex justify-content-center">
 						<input
 							type="text"
 							id="myInput"
-							placeholder={props.placeholder}
-							onChange={ev =>
-								optimizedFetch("PUT", JSON.stringify([...ev]))
-							}
+							placeholder={placeh}
+							onChange={ev => setData(ev.target.value)}
 						/>
+						<button
+							onClick={() => deleteAll()}
+							type="button"
+							className="btn btn-primary">
+							Delete all
+						</button>
 					</div>
 				</form>
 			</div>
@@ -85,9 +97,5 @@ const List = props => {
 			</ul>
 		</>
 	);
-};
-
-List.propTypes = {
-	placeholder: PropTypes.string
 };
 export default List;
